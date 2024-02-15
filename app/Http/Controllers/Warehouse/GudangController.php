64ce3387c6c6818049e38c2cf79c9825757890bd
sacred142 <agung.alfatah43@gmail.com>
2024-02-15 16:23:13 +0700

    gudang

--- app/Http/Controllers/Warehouse/GudangController.php
+++ app/Http/Controllers/Warehouse/GudangController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Warehouse;
 
 use App\Http\Controllers\Controller;
+use App\Models\Gudang;
+use Illuminate\Http\Request;
 
 class GudangController extends Controller
 {
     public function index()
     {
-        return view('warehouse.gudang.index');
+        $data = Gudang::all();
+        return view('warehouse.gudang.index', compact('data'));
     }
 
     public function add()
@@ -16,13 +19,44 @@ class GudangController extends Controller
         return view('warehouse.gudang.add');
     }
 
-    public function edit()
+    public function store(Request $request)
     {
-        return view('warehouse.gudang.edit');
+        $data = new Gudang();
+        $data->kode_gudang = $request->kode_gudang;
+        $data->nama_gudang = $request->nama_gudang;
+        $data->status = $request->status;
+        $data->save();
+        return back();
     }
 
-    public function detail()
+
+
+    public function edit($id)
     {
-        return view('warehouse.gudang.detail');
+        $data = Gudang::find($id);
+        return view('warehouse.gudang.edit', compact('data'));
+    }
+
+    public function update($id, Request $request)
+    {
+        $data = Gudang::find($id);
+        $data->kode_gudang = $request->kode_gudang;
+        $data->nama_gudang = $request->nama_gudang;
+        $data->status = $request->status;
+        $data->update();  
+        return back();
+    }
+
+    public function detail($id)
+    {
+        $data = Gudang::find($id);
+        return view('warehouse.gudang.detail', compact('data'));
+    }
+
+    public function delete($id)
+    {
+        $data = Gudang::find($id);
+        $data->delete();
+        return back();
     }
 }
