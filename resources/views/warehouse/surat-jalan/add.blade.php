0a8b0c94a6bd5ba0d8d87e5350d07c34cf8c1eb7
ARKA <mr.x685@gmail.com>
2024-02-21 17:00:14 +0700

    Update

--- resources/views/warehouse/surat-jalan/add.blade.php
+++ resources/views/warehouse/surat-jalan/add.blade.php
@@ -20,7 +20,7 @@
                             <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Status</label>
                             <select id="gudangAsal" name="gudang_asal_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                 <option value="">Tervalidasi</option>
-                                <option value="">Baru</option>
+                                <option value="">Pending</option>
                                 <option value="">Batal</option>
                             </select>
                         </div>
